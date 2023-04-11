fe22d9592b533a4a4cbffa8d90c746eac7589fe3
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-04-11 12:47:12 -0600

    "Cuarto Update"

--- views/paginas/informacion.php
+++ views/paginas/informacion.php
@@ -3,13 +3,13 @@
         <div class="imagen img-inovation">
             <h1>Innovation</h1>
         </div>
-        <div class="contenedor-info">
+        <div class="flex">
             <div class="contenedor-i texto-info">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae turpis vehicula, consectetur mi et, vestibulum dolor. Vestibulum id nunc at sapien euismod pulvinar. Maecenas interdum ullamcorper pretium. Donec vel tellus aliquam, imperdiet elit sit amet, blandit ligula. Praesent at efficitur velit. In et iaculis enim, vel malesuada est. Nulla tincidunt lobortis lectus, quis feugiat tellus dictum sit amet. Donec tempor nisl sit amet tortor tempus ullamcorper. Maecenas sed eros sed eros cursus dapibus. Aliquam sollicitudin ultricies diam sit amet suscipit. In nibh dolor, pretium id arcu a, varius tristique justo. Etiam elementum est ac placerat aliquam. Cras leo tortor, suscipit quis laoreet ac, finibus et dolor. Sed non dapibus risus.</p>
+                <p>At PX Consultores, we offer services of Engineering to analyze, design, simulate and prototype structural platforms for vehicles.</p>
             </div>
             
-            <div class="contenedor-i imagen-info">
-                <h1>IMAGEN</h1>
+            <div class="imagen-ino">
+                <img src="build/img/inovation-img.jpg">
             </div>
         </div>
     </div>
@@ -21,12 +21,12 @@
             <h1 class="blanco">GD&T</h1>
         </div>
         <div class="contenedor-info">
-            <div class="contenedor-i texto-info">
-                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam porttitor porttitor nisl et placerat. Nulla scelerisque massa ante, at viverra justo posuere non. Sed eleifend augue ut vestibulum iaculis. Sed mattis pretium arcu, et vehicula lorem molestie non. Cras sagittis vehicula lorem, vehicula dapibus elit lobortis varius. Fusce suscipit nisi a tellus varius malesuada. Cras maximus vel nibh nec dignissim. Proin scelerisque bibendum odio faucibus auctor. In varius, mi quis suscipit condimentum, enim augue egestas nunc, ac scelerisque dolor turpis eleifend diam. Phasellus ac odio venenatis, aliquet ipsum eget, efficitur arcu. Integer nec nisi a turpis porta dignissim. Cras ornare lobortis magna, gravida blandit dui vestibulum maximus.</p>
-            </div>
-            
             <div class="contenedor-i imagen-info">
                 <h1>IMAGEN</h1>
+            </div>
+            
+            <div class="contenedor-i texto-info">
+                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam porttitor porttitor nisl et placerat. Nulla scelerisque massa ante, at viverra justo posuere non. Sed eleifend augue ut vestibulum iaculis. Sed mattis pretium arcu, et vehicula lorem molestie non. Cras sagittis vehicula lorem, vehicula dapibus elit lobortis varius. Fusce suscipit nisi a tellus varius malesuada. Cras maximus vel nibh nec dignissim. Proin scelerisque bibendum odio faucibus auctor. In varius, mi quis suscipit condimentum, enim augue egestas nunc, ac scelerisque dolor turpis eleifend diam. Phasellus ac odio venenatis, aliquet ipsum eget, efficitur arcu. Integer nec nisi a turpis porta dignissim. Cras ornare lobortis magna, gravida blandit dui vestibulum maximus.</p>
             </div>
         </div>
     </div>
@@ -39,7 +39,7 @@
         </div>
         <div class="contenedor-info">
             <div class="contenedor-i texto-info">
-                <p>Network of Workshops</p>
+                <p>We have a network of workshops for the manufacture of components and the area for the assembly of your prototypes</p>
                 <p>Cut and Weld</p>
                 <p>Machining</p>
                 <p>Resin 3D print</p>
